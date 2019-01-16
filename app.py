6b78032f206a68806063c4b0cee9a57d4b7cc4bb
ironseiya <seiya.liulin@qq.com>
2019-01-17 05:58:10 +0800

    作业1

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 def hello():
     return "hello world"
 
+@app.route("/index")
+@app.route("/home")
+def index():
+    return "index page"
+
+
+@app.route("/user/<name>")
+def user_name(name='liulin'):
+    return "User: %s" % name
